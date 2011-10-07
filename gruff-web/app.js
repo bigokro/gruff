34f6dbdb0f975077c6f92213cfb336578a6b9d2c
firebus <russ@firebus.com>
2011-10-07 14:05:37 -0700

    clean up formatting

--- gruff-web/app.js
+++ gruff-web/app.js
@@ -18,24 +18,24 @@ var express = require('express')
 var app = module.exports = express.createServer();
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({secret: ':DP:DP:DP:DP'}));
-    app.use(express.methodOverride());
-    app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-    app.use(everyauth.middleware());
-    app.use(app.router);
-    app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({secret: ':DP:DP:DP:DP'}));
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(everyauth.middleware());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-    app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
