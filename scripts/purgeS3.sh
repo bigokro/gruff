24c01152f9765f21b6514d89b5f3317ef88838f0
root <root@domU-12-31-39-0B-28-6A.compute-1.internal>
2011-12-25 20:23:17 +0000

    better/stupder date logic

--- scripts/purgeS3.sh
+++ scripts/purgeS3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-DATE=`date -d yesterday +%Y%m`
+DATE=`date -d '63 days ago' +%Y%m`
 
+echo deleting backups wih prefix $DATE
+/opt/s3sync/s3cmd.rb list gruff_backup:$DATE
 /opt/s3sync/s3cmd.rb deleteall gruff_backup:$DATE
