2e39acc34ca591936763680656b7b8dd0fab702e
Timothy High <timothy.high@gmail.com>
2012-01-12 15:29:21 -0200

    Hacky method to get id from URL

--- gruff-web/public/javascript/canvas.js
+++ gruff-web/public/javascript/canvas.js
@@ -1,3 +1,5 @@
 require(["/backbone/gruff.js"], function(gruff) {
-  router = new Gruff.Routers.DebatesRouter({id: '4f03d5631544e424f7000001'});
+  var urlStr = window.location.href;
+  var id = urlStr.substring(urlStr.lastIndexOf('/') + 1);
+  router = new Gruff.Routers.DebatesRouter({id: id});
 });
