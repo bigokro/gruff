64acc831d7dfa635bd3df77b0672efd8ec04831a
Timothy High <timothy.high@gmail.com>
2012-01-15 01:27:40 -0200

    Fixed bug related to exports var and load order

--- gruff-web/public/javascript/canvas.js
+++ gruff-web/public/javascript/canvas.js
@@ -1,3 +1,4 @@
+var exports = this["GruffShared"] = {};
 require(["../models/identifiable", "../models/describable", "../lib/class_helper"], function(identifiable, describable, class_helper) {
   // TODO: Fix node/require deps to remove this hack
   require(["../models/debate"], function(debate) {
